aba3788e4d4aae4e9cc84ada5878adee30129bdc
Josh Breytspraak <133163031+JBreyt24@users.noreply.github.com>
2023-10-24 00:11:43 -0500

    Add files via upload

---
+++ Great_Number_Game/flask_app/controllers/users_controller.py
@@ -0,0 +1,64 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models import user_model # import entire file, rather than class, to avoid circular imports
+# As you add model files add them the the import above
+# This file is the second stop in Flask's thought process, here it looks for a route that matches the request
+import random
+
+
+# Create Users Controller
+
+
+
+# Read Users Controller
+
+@app.route('/')
+def index():
+    if "num" not in session:
+        session["num"] = random.randint(1, 100)
+    return render_template('index.html')
+
+
+@app.route('/guess', methods = ['POST'])
+def guess():
+    session['guess'] = int(request.form ['guess'])
+    return redirect('/')
+
+# Update Users Controller
+
+
+
+# Delete Users Controller
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+# Notes:
+# 1 - Use meaningful names
+# 2 - Do not overwrite function names
+# 3 - No matchy, no worky
+# 4 - Use consistent naming conventions 
+# 5 - Keep it clean
+# 6 - Test every little line before progressing
+# 7 - READ ERROR MESSAGES!!!!!!
+# 8 - Error messages are found in the browser and terminal
+
+
+
+
+# How to use path variables:
+# @app.route('/<int:id>')                                   The variable must be in the path within angle brackets
+# def index(id):                                            It must also be passed into the function as an argument/parameter
+#     user_info = user.User.get_user_by_id(id)              The it will be able to be used within the function for that route
+#     return render_template('index.html', user_info)
+
+# Converter -	Description
+# string -	Accepts any text without a slash (the default).
+# int -	Accepts integers.
+# float -	Like int but for floating point values.
+# path 	-Like string but accepts slashes.
+
+# Render template is a function that takes in a template name in the form of a string, then any number of named arguments containing data to pass to that template where it will be integrated via the use of jinja
+# Redirect redirects from one route to another, this should always be done following a form submission. Don't render on a form submission.
